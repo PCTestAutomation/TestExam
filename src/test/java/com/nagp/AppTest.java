92b4a45f9a853c2fb82e46fa1331b75703d4edf0
Prashant Chauhan <pchauhan@tillster.com>
2023-09-21 01:16:59 +0530

    test

---
+++ src/test/java/com/nagp/AppTest.java
@@ -0,0 +1,33 @@
+package com.nagp;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+	
+	@BeforeTest
+    public void setUp()
+    {
+        
+    }
+	
+    @Test
+    public void positiveScenario()
+    {
+        
+    }
+    
+    @Test
+    public void negativeScenario()
+    {
+        
+    }
+    
+    @AfterTest
+    public void tearDown()
+    {
+        
+    }
+}
